e478d69adddd6a3d736b9eac5677e678d7fca381
Gerard Kok <gerard@gerardkok.nl>
2019-01-28 20:47:46 +0100

    improveing 58

--- 051-075/058/euler58.rb
+++ 051-075/058/euler58.rb
@@ -1,20 +1,14 @@
 require 'prime'
 
 def number_of_corner_primes(spiral_index)
-  l = (2 * spiral_index - 1)**2
-  s = 2 * spiral_index - 2
-  r = [l - 3 * s, l - 2 * s, l - s].count(&:prime?)
+  a = 2 * spiral_index - 2
+  b = a**2 + 1
+  [b - a, b, b + a].count(&:prime?) # lower right corner is always a square, so never prime
 end
 
-$number_of_primes = Hash.new { |h, key| h[key] = number_of_corner_primes(key) }.update(1 => 0)
+$number_of_primes = Hash.new { |h, key| h[key] = number_of_corner_primes(key) + h[key - 1] }.update(1 => 0)
 
-def acc_number_of_primes(spiral_index)
-  (1..spiral_index).map { |n| $number_of_primes[n] }.reduce(:+)
-end
-
-index = 2.step.lazy.find { |n| 100 * acc_number_of_primes(n) / (4 * n - 3) < 10 }
-
-#index = (10000..11000).find { |n| a = acc_number_of_primes(n); d = (4 * n - 3); r = 100 * a / d; puts "n: #{n}, a: #{a}, d: #{d}, r: #{r}"; r < 10 }
+index = 2.step.lazy.find { |n| 100 * $number_of_primes[n] / (4 * n - 3) < 10 }
 
 answer = 2 * index - 1
 
