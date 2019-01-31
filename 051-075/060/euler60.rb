c63fbdc6d28f35b106bbd73738e63fd7779b0a33
Gerard Kok <gerard@gerardkok.nl>
2019-01-31 19:09:27 +0100

    faster algorithm for 60

--- 051-075/060/euler60.rb
+++ 051-075/060/euler60.rb
@@ -4,26 +4,18 @@ def concat(number1, number2)
   (number1.to_s + number2.to_s).to_i
 end
 
+def digitsum(number)
+  number.to_s.chars.map(&:to_i).reduce(:+)
+end
+
 def concatable_primes?(prime1, prime2)
+#  return false if ((digitsum(prime1) + digitsum(prime2)) % 3).zero?
   concat(prime1, prime2).prime? && concat(prime2, prime1).prime?
 end
 
 def concatable?(prime, set)
   set.all? { |p| concatable_primes?(p, prime) }
 end
-
-# $concatable_primes = Hash.new do |h, prime|
-#   h[prime] = h.keys.each_with_object([]) do |p, memo|
-#     if concatable_primes?(prime, p)
-#       puts "concatable: prime: #{prime}, p:#{p}"
-#       memo << [p, prime]
-#       h[p].select { |s| concatable?(prime, s) }.map { |i| i << prime }.each do |i|
-#         puts "prime: #{prime}, p: #{p}, h[#{p}]: #{h[p]}, i: #{i}"
-#         memo << i
-#       end
-#     end
-#   end
-# end
 
 $concatable_primes = Hash.new do |h, prime|
   c = h.keys.reduce([[]]) do |memo, p|
@@ -36,6 +28,7 @@ end
 
 Prime.each(10_000).each do |p|
   a = $concatable_primes[p]
+#  puts "[#{p}]: #{a}"
 end
 
 r = $concatable_primes.map { |_, s| s.max_by { |t| t.length } }.max_by(&:length)
