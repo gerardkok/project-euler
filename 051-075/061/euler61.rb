a190d9acae3a2d0e22b9e8f4afb3856e3ea326cd
Gerard Kok <gerard@gerardkok.nl>
2019-02-09 12:45:16 +0100

    added ruby for problem 61

--- 051-075/061/euler61.rb
+++ 051-075/061/euler61.rb
@@ -17,12 +17,26 @@ def polygonal(number, type)
   end
 end
 
-figurates = 1.step.each_with_object({}) do |n, result|
-  numbers = POLYGONAL_TYPES.each_with_object({}) { |p, r| r[p] = polygonal(n, p) }
-  break result if numbers.values.all? { |p| p > 9_999 }
+def cycle_of_six(start, figurates, cycle_so_far = [])
+  return cycle_so_far if cycle_so_far.length == 6 && cycle_so_far.first.first == start
 
-  numbers.select { |_, p| p.between?(1_000, 9_999) }.each do |t, p|
-    _, f, l = p.to_s.partition(%r{..})
+  figurates[start].each do |type, next_steps|
+    next if cycle_so_far.map(&:last).include?(type)
+
+    next_steps.each do |step|
+      c = cycle_of_six(step, figurates, cycle_so_far + [[start, type]])
+      return c if c
+    end
+  end
+  nil
+end
+
+figurates = 1.step.each_with_object({}) do |n, result|
+  numbers = POLYGONAL_TYPES.each_with_object({}) { |p, r| r[p] = polygonal(n, p).to_s }
+  break result if numbers.values.map(&:length).all? { |l| l > 4 }
+
+  numbers.select { |_, p| p.length == 4 }.each do |t, p|
+    f, l = p.scan(%r{(..)(..)}).sample
     next if l.start_with?('0')
 
     result[f] ||= {}
@@ -30,4 +44,11 @@ figurates = 1.step.each_with_object({}) do |n, result|
   end
 end
 
-puts figurates.to_s
+cycle = figurates.keys.each do |k|
+  c = cycle_of_six(k, figurates)
+  break c if c
+end
+
+answer = cycle.map(&:first).map(&:to_i).reduce(0) { |sum, n| sum + n * 100 + n }
+
+puts answer
