50044c06044098c7988b579995ff4b04a8bbd591
Gerard Kok <gerard@gerardkok.nl>
2019-02-12 07:47:18 +0100

    minor fixes

--- 051-075/064/euler64.rb
+++ 051-075/064/euler64.rb
@@ -1,12 +1,15 @@
-def recurring_cycle_length(number, m = 0, d = 1, a = nil)
+def recurring_cycle_length(number, s = nil, m = 0, d = 1, a = nil)
   # see https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Algorithm
-  s = Math.sqrt(number).to_i
-  return 0 if s**2 == number || (a ||= s) == 2 * s # number is perfect square or algorithm terminates
+  unless s
+    return 0 if (s = Math.sqrt(number).to_i)**2 == number # number is perfect square
+  end
+
+  return 0 if (a ||= s) == 2 * s # algorithm terminates normally
 
   m = d * a - m
   d = (number - m**2) / d
   a = ((s + m) / d).to_i
-  recurring_cycle_length(number, m, d, a) + 1
+  recurring_cycle_length(number, s, m, d, a) + 1
 end
 
 answer = (1..10_000).map { |n| recurring_cycle_length(n) }.count(&:odd?)
