531708a198ab94847b68a182fbd7869449822aa4
Gerard Kok <gerard@gerardkok.nl>
2019-02-13 16:36:27 +0100

    better solution for 65

--- 051-075/065/euler65.rb
+++ 051-075/065/euler65.rb
@@ -1,22 +1,10 @@
 def e_continued_fraction(index)
-  return 2 if index.zero?
-
   d, m = index.divmod(3)
-  if m == 2
-    (d + 1) * 2
-  else
-    1
-  end
+  m.zero? ? d * 2 : 1
 end
 
-def e_convergent_fraction(index, upto)
-  return e_continued_fraction(index) if index == upto
+numerators = Hash.new { |h, i| h[i] = e_continued_fraction(i) * h[i - 1] + h[i - 2] }.update(1 => 2, 2 => 3)
 
-  e_continued_fraction(index) + Rational(1, e_convergent_fraction(index + 1, upto))
-end
-
-hundredth_term = e_convergent_fraction(0, 99)
-
-answer = hundredth_term.numerator.to_s.chars.map(&:to_i).reduce(:+)
+answer = numerators[100].to_s.chars.map(&:to_i).reduce(:+)
 
 puts answer
