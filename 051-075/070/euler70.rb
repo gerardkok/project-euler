9008e74f920e782bf8108bad7b5d364e00d278c4
Gerard Kok <gerard@gerardkok.nl>
2019-02-17 16:29:31 +0100

    fix in comments

--- 051-075/070/euler70.rb
+++ 051-075/070/euler70.rb
@@ -9,7 +9,7 @@ end
 # https://math.stackexchange.com/questions/1437918/show-that-n-m-is-a-multiple-of-9-when-n-and-m-have-same-digits)
 # i.e. (n - 𝜑(n)) % 9 = 0, so in this case: (pq - (p - 1)(q - 1)) % 9 = 0
 # simplifying gives: (pq - pq + p + q - 1) % 9 = 0, so (p + q) % 9 = 1
-# note that not all numbers that differ by 9 are permutations of each other,
+# note that not all numbers whose difference is divisible by 9 are permutations of each other,
 # so we still have to verify that 𝜑(n) is a permutation of n
 permuted_totients = Prime.take_while { |p| p < Math.sqrt(1E7) }.each_with_object({}) do |p, result|
   Prime.take_while { |q| p * q < 1E7 }.drop_while { |q| q < p }.each do |q|
