64469ca401f825345c1f027a30fc02cc5d27e13f
Gerard Kok <gerard@gerardkok.nl>
2019-02-23 16:43:08 +0100

    improved readability

--- 076-100/077/euler77.rb
+++ 076-100/077/euler77.rb
@@ -1,14 +1,14 @@
 require 'prime'
 
-def combine(upto, primes = Prime.each(upto).to_a)
+def count_prime_summations(upto, primes = Prime.each(upto).to_a)
   return 1 if upto.zero?
   return 0 if upto.negative?
 
   primes.each_with_index.reduce(0) do |sum, (p, i)|
-    sum + combine(upto - p, primes[i..-1])
+    sum + count_prime_summations(upto - p, primes[0..i])
   end
 end
 
-answer = 2.step.find { |n| combine(n) > 5_000 }
+answer = 2.step.find { |n| count_prime_summations(n) > 5_000 }
 
 puts answer
