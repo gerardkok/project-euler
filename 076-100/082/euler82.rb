e7ec85910384a4dfab6df265af01fdfc02811478
Gerard Kok <gerard@gerardkok.nl>
2019-02-27 23:12:45 +0100

    minor simplification

--- 076-100/082/euler82.rb
+++ 076-100/082/euler82.rb
@@ -3,8 +3,7 @@ require 'csv'
 def min_path_length(matrix)
   size = matrix.length # assuming square matrix
   last_row_lengths = matrix.drop(1).reduce(matrix.first) do |result, row|
-    (0...size).each_with_object(Array.new(size, Float::INFINITY)) do |i, r|
-      r[i] = row[i] + result[i] if row[i] + result[i] < r[i]
+    (0...size).each_with_object(Array.new(size) { |i| row[i] + result[i] }) do |i, r|
       (i + 1...size).each do |j|
         range_sum = row[i..j].reduce(:+)
         r[i] = range_sum + result[j] if range_sum + result[j] < r[i]
