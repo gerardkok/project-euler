35b1053696fb7650aefc715aca3cc6626b36f41b
Gerard Kok <gerard@gerardkok.nl>
2019-02-27 11:09:54 +0100

    simplified 81

--- 076-100/081/euler81.rb
+++ 076-100/081/euler81.rb
@@ -4,8 +4,8 @@ def min_path_length(matrix)
   size = matrix.length # assuming square matrix
   initial = Array.new(size) { |i| i.zero? ? 0 : Float::INFINITY }
   lengths = (0...size).reduce(initial) do |result, row|
-    (0...size).each_cons(2).reduce([result.first + matrix[row].first]) do |row_so_far, (left, right)|
-      row_so_far << [row_so_far[left], result[right]].min + matrix[row][right]
+    (1...size).reduce([result.first + matrix[row].first]) do |row_so_far, i|
+      row_so_far << [row_so_far.last, result[i]].min + matrix[row][i]
     end
   end
   lengths.last
