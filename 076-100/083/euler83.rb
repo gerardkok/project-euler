bb26320ff56eb6b978b0e5cbeda9cdad25bf53cd
Gerard Kok <gerard@gerardkok.nl>
2019-02-27 19:19:18 +0100

    initial version of 83

--- 076-100/083/euler83.rb
+++ 076-100/083/euler83.rb
@@ -1,7 +1,28 @@
-matrix = [
-  [131, 672, 234, 103, 18],
-  [201, 96, 342, 965, 150],
-  [630, 803, 746, 422, 111],
-  [537, 699, 497, 121, 956],
-  [805, 732, 524, 37, 331]
-]
+require 'csv'
+
+def dijkstra(matrix)
+  size = matrix.length
+  unvisited = [[0, 0]]
+  distances = Array.new(size) { |r| Array.new(size) { |c| (r.zero? && c.zero?) ? matrix[0][0] : Float::INFINITY } }
+
+  r, c = unvisited.shift
+  until r == size - 1 && c == size - 1
+    neighbours = [[r - 1, c], [r + 1, c], [r, c - 1], [r, c + 1]].select { |x, y| x.between?(0, size - 1) && y.between?(0, size - 1) }
+    neighbours.each do |rn, cn|
+      d = distances[r][c] + matrix[rn][cn]
+      next unless distances[rn][cn] > d
+
+      distances[rn][cn] = d
+      unvisited << [rn, cn]
+    end
+    unvisited.sort_by! { |x, y| distances[x][y] }
+    r, c = unvisited.shift
+  end
+  distances[r][c]
+end
+
+matrix = CSV.read('input83.txt').map { |line| line.map(&:to_i) }
+
+answer = dijkstra(matrix)
+
+puts answer
