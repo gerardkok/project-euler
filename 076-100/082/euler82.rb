924ac422faa23b82fac4e26714a07b767eca7bec
Gerard Kok <gerard@gerardkok.nl>
2019-02-27 23:05:26 +0100

    preformance improvement for 82

--- 076-100/082/euler82.rb
+++ 076-100/082/euler82.rb
@@ -1,16 +1,15 @@
 require 'csv'
 
-def row_range_sum(row, i, j)
-  lower, upper = (j < i) ? [j, i] : [i, j]
-  row[lower..upper].reduce(:+)
-end
-
 def min_path_length(matrix)
   size = matrix.length # assuming square matrix
-  initial = Array.new(size, 0)
-  last_row_lengths = matrix.reduce(initial) do |result, row|
-    (0...size).map do |i|
-      (0...size).map { |j| row_range_sum(row, i, j) + result[j] }.min
+  last_row_lengths = matrix.drop(1).reduce(matrix.first) do |result, row|
+    (0...size).each_with_object(Array.new(size, Float::INFINITY)) do |i, r|
+      r[i] = row[i] + result[i] if row[i] + result[i] < r[i]
+      (i + 1...size).each do |j|
+        range_sum = row[i..j].reduce(:+)
+        r[i] = range_sum + result[j] if range_sum + result[j] < r[i]
+        r[j] = range_sum + result[i] if range_sum + result[i] < r[j]
+      end
     end
   end
   last_row_lengths.min
