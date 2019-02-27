528d3773abe4a11c733fe3bbc945e8c66b95cec5
Gerard Kok <gerard@gerardkok.nl>
2019-02-27 19:28:13 +0100

    improved 83

--- 076-100/083/euler83.rb
+++ 076-100/083/euler83.rb
@@ -1,14 +1,16 @@
 require 'csv'
 
-def dijkstra(matrix)
-  size = matrix.length
+def neighbours(r, c, max)
+  [[r - 1, c], [r + 1, c], [r, c - 1], [r, c + 1]].select { |x, y| x.between?(0, max) && y.between?(0, max) }
+end
+
+def min_path_length(matrix)
+  size = matrix.length # assuming square matrix
   unvisited = [[0, 0]]
   distances = Array.new(size) { |r| Array.new(size) { |c| (r.zero? && c.zero?) ? matrix[0][0] : Float::INFINITY } }
 
-  r, c = unvisited.shift
-  until r == size - 1 && c == size - 1
-    neighbours = [[r - 1, c], [r + 1, c], [r, c - 1], [r, c + 1]].select { |x, y| x.between?(0, size - 1) && y.between?(0, size - 1) }
-    neighbours.each do |rn, cn|
+  until (r, c = unvisited.shift) == [size - 1, size - 1]
+    neighbours(r, c, size - 1).each do |rn, cn|
       d = distances[r][c] + matrix[rn][cn]
       next unless distances[rn][cn] > d
 
@@ -16,13 +18,12 @@ def dijkstra(matrix)
       unvisited << [rn, cn]
     end
     unvisited.sort_by! { |x, y| distances[x][y] }
-    r, c = unvisited.shift
   end
   distances[r][c]
 end
 
 matrix = CSV.read('input83.txt').map { |line| line.map(&:to_i) }
 
-answer = dijkstra(matrix)
+answer = min_path_length(matrix)
 
 puts answer
