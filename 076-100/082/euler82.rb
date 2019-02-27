995a66fc7e750356d549948bad2ae4ba1be277e9
Gerard Kok <gerard@gerardkok.nl>
2019-02-27 11:17:54 +0100

    better variable name

--- 076-100/082/euler82.rb
+++ 076-100/082/euler82.rb
@@ -8,12 +8,12 @@ end
 def min_path_length(matrix)
   size = matrix.length # assuming square matrix
   initial = Array.new(size, 0)
-  lengths = matrix.reduce(initial) do |result, row|
+  last_row_lengths = matrix.reduce(initial) do |result, row|
     (0...size).map do |i|
       (0...size).map { |j| row_range_sum(row, i, j) + result[j] }.min
     end
   end
-  lengths.min
+  last_row_lengths.min
 end
 
 matrix = CSV.read('input82.txt').map { |line| line.map(&:to_i) }.transpose
