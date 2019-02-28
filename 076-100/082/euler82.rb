f6826396b7982ac44a725f8781ecd005e4d01197
Gerard Kok <gerard@gerardkok.nl>
2019-02-28 07:37:47 +0100

    minor performance improvement

--- 076-100/082/euler82.rb
+++ 076-100/082/euler82.rb
@@ -4,8 +4,9 @@ def min_path_length(matrix)
   size = matrix.length # assuming square matrix
   last_row_lengths = matrix.reduce do |result, row|
     (0...size).each_with_object(Array.new(size) { |i| row[i] + result[i] }) do |i, r|
+      range_sum = row[i]
       (i + 1...size).each do |j|
-        range_sum = row[i..j].reduce(:+)
+        range_sum += row[j]
         r[i] = range_sum + result[j] if range_sum + result[j] < r[i]
         r[j] = range_sum + result[i] if range_sum + result[i] < r[j]
       end
