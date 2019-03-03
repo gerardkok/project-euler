753b0c25af915fa8afe0b330404b345111efb698
Gerard Kok <gerard@gerardkok.nl>
2019-03-03 15:37:02 +0100

    a working version

--- 076-100/084/markov.rb
+++ 076-100/084/markov.rb
@@ -6,10 +6,9 @@ SQUARES = [:GO, :A1, :CC1, :A2, :T1, :R1, :B1, :CH1, :B2, :B3,
   :FP, :E1, :CH2, :E2, :E3, :R3, :F1, :F2, :U2, :F3,
   :G2J, :G1, :G2, :CC3, :G3, :R4, :CH3, :H1, :T2, :H2].freeze
 
-THREE_DOUBLES_PROBABILITY = Rational(1, DICE_SIZE)**3
-NOT_THREE_DOUBLES_PROBABILITY = 1 - THREE_DOUBLES_PROBABILITY
+THREE_DOUBLES_PROBABILITY = 3.0 / DICE_SIZE**4 # 3 * DICE_SIZE / DICE_SIZE**5
 
-DICE_PROBABILITY = (2..2*DICE_SIZE).map { |i| DICE_SIZE - (i - DICE_SIZE - 1.0).abs }.map { |i| i * NOT_THREE_DOUBLES_PROBABILITY / DICE_SIZE**2 }
+DICE_PROBABILITY = (2..2*DICE_SIZE).map { |i| DICE_SIZE - (i - DICE_SIZE - 1).abs }.map { |i| i.odd? ? (64.0 * i - 3) : 64.0 * i }.map { |i| i / (DICE_SIZE**5) }
 
 def next_railway(chance_square)
   case chance_square
@@ -23,57 +22,65 @@ def next_utility(community_chest_square)
   (community_chest_square == :CH2) ? :U2 : :U1
 end
 
-def square_probability(row, square, dice_probablity)
-#  puts "square_probability(#{row}, square, dice_probablity)"
-  case SQUARES[square]
-  when :G2J
-    row[SQUARES.index(:JAIL)] += dice_probablity
-  when :CC1, :CC2, :CC3
-    row[SQUARES.index(:GO)] += dice_probablity / 16
-    row[SQUARES.index(:JAIL)] += dice_probablity / 16
-    row[square] += 14 * dice_probablity / 16
-  when :CH1, :CH2, :CH3
-    row[SQUARES.index(:GO)] += dice_probablity / 16
-    row[SQUARES.index(:JAIL)] += dice_probablity / 16
-    row[SQUARES.index(:C1)] += dice_probablity / 16
-    row[SQUARES.index(:E3)] += dice_probablity / 16
-    row[SQUARES.index(:H2)] += dice_probablity / 16
-    row[SQUARES.index(:R1)] += dice_probablity / 16
-    row[SQUARES.index(next_railway(SQUARES[square]))] += 2 * dice_probablity / 16
-    row[SQUARES.index(next_utility(SQUARES[square]))] += dice_probablity / 16
-    y = (square < 3) ? square + 37 : square - 3
-    square_probability(row, y, dice_probablity)
-    row[square] += 6 * dice_probablity / 16
-  else
-    row[square] += dice_probablity
-  end
+def community_chest?(square)
+  [:CC1, :CC2, :CC3].include?(square)
 end
 
-def row_probabilities(row_index)
-  row = Array.new(SQUARES.length, 0)
-  row[SQUARES.index(:JAIL)] += THREE_DOUBLES_PROBABILITY
+def chance?(square)
+  [:CH1, :CH2, :CH3].include?(square)
+end
 
+roll_matrix_rows = Array.new(SQUARES.length) { Array.new(SQUARES.length, 0.0) }
+
+(0...SQUARES.length).each do |column|
   DICE_PROBABILITY.each_with_index do |d, i|
-    square = (row_index + i + 2) % SQUARES.length
-    square_probability(row, square, d)
+    row = (column + i + 2) % SQUARES.length
+    roll_matrix_rows[row][column] = d
   end
-#  puts "row: #{row}"
-  row
+  roll_matrix_rows[SQUARES.index(:JAIL)][column] += THREE_DOUBLES_PROBABILITY
 end
 
-rows = (0...SQUARES.length).reduce([]) do |rows, r|
-  rows << row_probabilities(r)
+roll_matrix = Matrix[*roll_matrix_rows]
+
+special_transition_matrix_rows = Array.new(SQUARES.length) { Array.new(SQUARES.length, 0.0) }
+
+(0...SQUARES.length).each do |column|
+  if SQUARES[column] == :G2J
+    special_transition_matrix_rows[SQUARES.index(:JAIL)][column] = 1.0
+  elsif chance?(SQUARES[column])
+    [:GO, :JAIL, :C1, :E3, :H2, :R1].each do |card|
+      special_transition_matrix_rows[SQUARES.index(card)][column] = 1.0 / 16
+    end
+    three_squares_back = (column - 3) % SQUARES.length
+    if community_chest?(SQUARES[three_squares_back])
+      special_transition_matrix_rows[three_squares_back][column] += (1.0 / 16) * (14.0 / 16)
+      [:GO, :JAIL].each do |card|
+        special_transition_matrix_rows[SQUARES.index(card)][column] += (1.0 / 16) * (1.0 / 16)
+      end
+    else
+      special_transition_matrix_rows[three_squares_back][column] += (1.0 / 16)
+    end
+    special_transition_matrix_rows[SQUARES.index(next_railway(column))][column] += 1.0 / 16
+    special_transition_matrix_rows[SQUARES.index(next_utility(column))][column] += 1.0 / 16
+    special_transition_matrix_rows[column][column] = 7.0 / 16
+  elsif community_chest?(SQUARES[column])
+    [:GO, :JAIL].each do |card|
+      special_transition_matrix_rows[SQUARES.index(card)][column] = 1.0 / 16
+    end
+    special_transition_matrix_rows[column][column] = 14.0 / 16
+  else
+    special_transition_matrix_rows[column][column] = 1.0
+  end
 end
 
-matrix = Matrix[*rows]
+special_transition_matrix = Matrix[*special_transition_matrix_rows]
 
-(0...SQUARES.length).each do |i|
-  row_sum = matrix.row(i).reduce(0, :+)
-  column_sum = matrix.column(i).reduce(0, :+)
-  puts "[#{i}] row: #{row_sum}, column: #{column_sum}"
-end
+transition_matrix = special_transition_matrix * roll_matrix
 
-m = matrix**50
-#puts m.to_s
+v, = transition_matrix.eigen
 
-puts m.row(0).each_with_index.max_by(3) { |v, _| v }
+normalized_vector = v.real.column(0).normalize
+
+puts normalized_vector.to_s
+
+puts normalized_vector.each_with_index.max_by(3) { |v, _| v }
