ac09c621c41eb3c7ceabc1f574f6862832abc49e
Gerard Kok <gerard@gerardkok.nl>
2019-03-04 21:47:04 +0100

    entirely reverting to monte carlo simulation

--- 076-100/084/euler84.rb
+++ 076-100/084/euler84.rb
@@ -1,93 +1,110 @@
-require 'matrix'
+class Dice
+  attr_reader :sum, :double
+  alias double? double
 
-FACES = 4
-SQUARES = [:GO, :A1, :CC1, :A2, :T1, :R1, :B1, :CH1, :B2, :B3,
-           :JAIL, :C1, :U1, :C2, :C3, :R2, :D1, :CC2, :D2, :D3,
-           :FP, :E1, :CH2, :E2, :E3, :R3, :F1, :F2, :U2, :F3,
-           :G2J, :G1, :G2, :CC3, :G3, :R4, :CH3, :H1, :T2, :H2].freeze
+  FACES = 4
 
-THREE_DOUBLES_PROBABILITY = (FACES.to_f - 1) / FACES**4
-
-roll_probabilities = (1..FACES).each_with_object({}) do |i, acc|
-  p = (i.odd? ? (FACES.to_f**3 * i - (FACES - 1)) : FACES.to_f**3 * i) / FACES**5
-  [i, 2 * FACES - i].uniq.map(&:next).each do |k|
-    acc[k] = p
+  def initialize
+    one = rand(FACES).next
+    other = rand(FACES).next
+    @sum = one + other
+    @double = (one == other)
   end
 end
 
-def next_railway(chance_square)
-  case chance_square
-  when :CH1 then :R2
-  when :CH2 then :R3
-  else :R1
+class Board
+  SQUARES = [:GO, :A1, :CC1, :A2, :T1, :R1, :B1, :CH1, :B2, :B3,
+             :JAIL, :C1, :U1, :C2, :C3, :R2, :D1, :CC2, :D2, :D3,
+             :FP, :E1, :CH2, :E2, :E3, :R3, :F1, :F2, :U2, :F3,
+             :G2J, :G1, :G2, :CC3, :G3, :R4, :CH3, :H1, :T2, :H2].freeze
+  CC = [:GO, :JAIL, *Array.new(14, :ignore)].freeze
+  CH = [:GO, :JAIL, :C1, :E3, :H2, :R1, :next_R, :next_R, :next_U, :back_3, *Array.new(6, :ignore)].freeze
+
+  def initialize
+    @position = :GO
+    @cc_deck = CC.shuffle
+    @ch_deck = CH.shuffle
+    @consecutive_doubles = 0
   end
-end
 
-def next_utility(community_chest_square)
-  (community_chest_square == :CH2) ? :U2 : :U1
-end
-
-def community_chest?(square)
-  [:CC1, :CC2, :CC3].include?(square)
-end
-
-def chance?(square)
-  [:CH1, :CH2, :CH3].include?(square)
-end
-
-roll_matrix_rows = Array.new(SQUARES.length) { Array.new(SQUARES.length, 0.0) }
-
-(0...SQUARES.length).each do |column|
-  roll_probabilities.each do |roll, p|
-    row = (column + roll) % SQUARES.length
-    roll_matrix_rows[row][column] = p
+  def index
+    SQUARES.index(@position)
   end
-  roll_matrix_rows[SQUARES.index(:JAIL)][column] += THREE_DOUBLES_PROBABILITY
-end
 
-roll_matrix = Matrix[*roll_matrix_rows]
+  def advance(steps)
+    SQUARES[(index + steps) % SQUARES.length]
+  end
 
-special_transition_matrix_rows = Array.new(SQUARES.length) { Array.new(SQUARES.length, 0.0) }
+  def retreat(steps)
+    SQUARES[(index + SQUARES.length - steps) % SQUARES.length]
+  end
 
-(0...SQUARES.length).each do |column|
-  if SQUARES[column] == :G2J
-    special_transition_matrix_rows[SQUARES.index(:JAIL)][column] = 1.0
-  elsif chance?(SQUARES[column])
-    [:GO, :JAIL, :C1, :E3, :H2, :R1].each do |card|
-      special_transition_matrix_rows[SQUARES.index(card)][column] = 1.0 / 16
-    end
-    three_squares_back = (column - 3) # % SQUARES.length
-    if community_chest?(SQUARES[three_squares_back])
-      special_transition_matrix_rows[three_squares_back][column] += (1.0 / 16) * (14.0 / 16)
-      [:GO, :JAIL].each do |card|
-        special_transition_matrix_rows[SQUARES.index(card)][column] += (1.0 / 16) * (1.0 / 16)
-      end
+  def roll
+    dice = Dice.new
+    @consecutive_doubles = dice.double? ? @consecutive_doubles + 1 : 0
+    if @consecutive_doubles == 3
+      go_to_jail
     else
-      special_transition_matrix_rows[three_squares_back][column] += 1.0 / 16
+      @position = advance(dice.sum)
+      chance_card if chance?
+      community_chest_card if community_chest?
+      go_to_jail if @position == :G2J
     end
-    special_transition_matrix_rows[SQUARES.index(next_railway(column))][column] = (1.0 / 16)
-    special_transition_matrix_rows[SQUARES.index(next_utility(column))][column] = (1.0 / 16)
-    special_transition_matrix_rows[column][column] = 7.0 / 16
-  elsif community_chest?(SQUARES[column])
-    [:GO, :JAIL].each do |card|
-      special_transition_matrix_rows[SQUARES.index(card)][column] = (1.0 / 16)
+  end
+
+  def go_to_jail
+    @consecutive_doubles = 0
+    @position = :JAIL
+  end
+
+  def chance?
+    [:CH1, :CH2, :CH3].include?(@position)
+  end
+
+  def community_chest?
+    [:CC1, :CC2, :CC3].include?(@position)
+  end
+
+  def next_railway
+    case @position
+    when :CH1 then :R2
+    when :CH2 then :R3
+    else :R1
     end
-    special_transition_matrix_rows[column][column] = 14.0 / 16
-  else
-    special_transition_matrix_rows[column][column] = 1.0
+  end
+
+  def next_utility
+    (@position == :CH2) ? :U2 : :U1
+  end
+
+  def chance_card
+    card = @ch_deck.first
+    @ch_deck.rotate!
+    if [:GO, :JAIL, :C1, :E3, :H2, :R1].include?(card)
+      @position = card
+    elsif card == :next_R
+      @position = next_railway
+    elsif card == :next_U
+      @position = next_utility
+    elsif card == :back_3
+      @position = retreat(3)
+    end
+  end
+
+  def community_chest_card
+    card = @cc_deck.first
+    @cc_deck.rotate!
+    @position = card unless card == :ignore
   end
 end
 
-special_transition_matrix = Matrix[*special_transition_matrix_rows]
+board = Board.new
 
-transition_matrix = special_transition_matrix * roll_matrix
+freqs = 10_000_000.times.each_with_object(Hash.new(0)) do |_, result|
+  board.roll
+  result[board.index] += 1
+end
 
-v, = transition_matrix.eigen
-
-dominant_vector = v.column(0)
-sum = dominant_vector.reduce(:+)
-normalized_vector = dominant_vector.map { |x| x / sum }
-
-answer = normalized_vector.each_with_index.max_by(3, &:first).map(&:last).map { |n| '%02d' % n }.join
+answer = freqs.max_by(3, &:last).map(&:first).map { |n| '%02d' % n }.join
 
 puts answer
