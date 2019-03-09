f8c7302ca0c16ccd94cc75f30282b1b8ea22ea9a
Gerard Kok <gerard@gerardkok.nl>
2019-03-09 21:10:07 +0100

    cosmetic

--- 076-100/087/euler87.rb
+++ 076-100/087/euler87.rb
@@ -4,8 +4,8 @@ require 'set'
 MAX = 50_000_000
 
 combinations = Prime.take_while { |p| p**4 < MAX }.map { |p| p**4 }.each_with_object([].to_set) do |f, result|
-  Prime.take_while { |c| c**3 < MAX - f }.map { |p| p**3 }.each do |c|
-    Prime.take_while { |s| s**2 < MAX - f - c }.map { |p| p**2 }.each do |s|
+  Prime.take_while { |p| p**3 < MAX - f }.map { |p| p**3 }.each do |c|
+    Prime.take_while { |p| p**2 < MAX - f - c }.map { |p| p**2 }.each do |s|
       result << f + c + s
     end
   end
