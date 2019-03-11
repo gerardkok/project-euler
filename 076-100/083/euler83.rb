5eb923b419b92223aaa8a72a15a57504bbc9a216
Gerard Kok <gerard@gerardkok.nl>
2019-03-11 20:35:46 +0100

    83 using priority queue

--- 076-100/083/euler83.rb
+++ 076-100/083/euler83.rb
@@ -1,24 +1,102 @@
 require 'csv'
 
-def neighbours(r, c, max)
-  [[r - 1, c], [r + 1, c], [r, c - 1], [r, c + 1]].select { |x, y| x.between?(0, max) && y.between?(0, max) }
-end
+class PriorityQueue
+  def initialize(size)
+    @elements = [nil]
+    @distances = Array.new(size) { |_| Array.new(size, Float::INFINITY) }
+  end
 
-def min_path_length(matrix)
-  size = matrix.length # assuming square matrix
-  unvisited = [[0, 0]]
-  distances = Array.new(size) { |r| Array.new(size) { |c| (r.zero? && c.zero?) ? matrix[0][0] : Float::INFINITY } }
+  def push(row, column, distance)
+    @elements << [row, column]
+    @distances[row][column] = distance
+    float
+  end
 
-  until (r, c = unvisited.shift) == [size - 1, size - 1]
-    neighbours(r, c, size - 1).each do |rn, cn|
-      d = distances[r][c] + matrix[rn][cn]
-      next unless distances[rn][cn] > d
+  def length
+    @elements.size - 1
+  end
 
-      distances[rn][cn] = d
-      unvisited.push([rn, cn]).sort_by! { |x, y| distances[x][y] }
+  def empty?
+    length.zero?
+  end
+
+  def pop
+    swap(1, length)
+    e = @elements.pop
+    sink
+    e
+  end
+
+  def [](row, column)
+    @distances[row][column]
+  end
+
+  private
+
+  def float
+    k = length
+    while (j = k) > 1 && heavier?(k >> 1, j)
+      k >>= 1
+      swap(j, k)
     end
   end
-  distances[size - 1][size - 1]
+
+  def sink
+    k = 1
+    while (j = 2 * k) <= length
+      j += 1 if j < length && heavier?(j, j + 1)
+      return if lighter?(k, j)
+
+      swap(k, j)
+      k = j
+    end
+  end
+
+  def swap(i, j)
+    @elements[i], @elements[j] = @elements[j], @elements[i]
+  end
+
+  def heavier?(i, j)
+    self[*@elements[i]] > self[*@elements[j]]
+  end
+
+  def lighter?(i, j)
+    self[*@elements[i]] < self[*@elements[j]]
+  end
+end
+
+def left_neighbour(r, c, unvisited, matrix)
+  d = unvisited[r, c] + matrix[r - 1][c]
+  unvisited.push(r - 1, c, d) if unvisited[r - 1, c] > d
+end
+
+def right_neighbour(r, c, unvisited, matrix)
+  d = unvisited[r, c] + matrix[r + 1][c]
+  unvisited.push(r + 1, c, d) if unvisited[r + 1, c] > d
+end
+
+def upper_neighbour(r, c, unvisited, matrix)
+  d = unvisited[r, c] + matrix[r][c - 1]
+  unvisited.push(r, c - 1, d) if unvisited[r, c - 1] > d
+end
+
+def lower_neighbour(r, c, unvisited, matrix)
+  d = unvisited[r, c] + matrix[r][c + 1]
+  unvisited.push(r, c + 1, d) if unvisited[r, c + 1] > d
+end
+
+def min_path_length(matrix, size = matrix.length)
+  unvisited = PriorityQueue.new(size)
+  unvisited.push(0, 0, matrix[0][0])
+  max = size - 1
+
+  until (r, c = unvisited.pop) == [max, max]
+    left_neighbour(r, c, unvisited, matrix) unless r.zero?
+    right_neighbour(r, c, unvisited, matrix) unless r == max
+    upper_neighbour(r, c, unvisited, matrix) unless c.zero?
+    lower_neighbour(r, c, unvisited, matrix) unless c == max
+  end
+  unvisited[max, max]
 end
 
 matrix = CSV.read('input83.txt').map { |line| line.map(&:to_i) }
