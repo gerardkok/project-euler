4fe1b39959cdfc7d11b741cd11d249e29619c28f
Gerard Kok <gerard@gerardkok.nl>
2019-03-13 19:07:32 +0100

    improving 88

--- 076-100/088/euler88.rb
+++ 076-100/088/euler88.rb
@@ -9,31 +9,23 @@ factors = (2..MAX_N).each_with_object(Array.new(MAX_N + 1) { |_| [] }) do |n, ac
   end
 end
 
-#puts factors.to_s
-
-factorizations = (1..MAX_N).each_with_object({}) do |n, acc|
-  acc[n] = [[n]]
-  factors[n].each do |f|
-    acc[n / f].each do |i|
-      acc[n] << i + [f] if i.none? { |j| j < f }
-    end
-  end
+factorizations = (1..MAX_N).each_with_object(Array.new(MAX_N + 1) { |i| [[i]] }) do |n, acc|
+  acc[n] += factors[n].flat_map { |f| acc[n / f].select { |i| i.none? { |j| j < f } }.map { |i| i + [f] } }
 end
-
-#puts factorizations.to_s
 
 def k(factorization)
   factorization.length - factorization.reduce(:+) + factorization.reduce(:*)
 end
 
-k = factorizations.each_with_object(Array.new(MAX_K + 1, Float::INFINITY)) do |(i, f), acc|
+minimal_product_sum_numbers = factorizations.each_with_index.each_with_object(Array.new(MAX_K + 1, Float::INFINITY)) do |(f, i), acc|
   f.each do |factorization|
     next if factorization.length == 1
-    k_fact = k(factorization)
-    acc[k_fact] = i if k_fact <= MAX_K && i < acc[k_fact]
+
+    k = k(factorization)
+    acc[k] = i if k <= MAX_K && i < acc[k]
   end
 end
 
-#puts k.to_s
+answer = minimal_product_sum_numbers.select { |k| k < Float::INFINITY }.uniq.reduce(:+)
 
-puts k.select { |i| i < Float::INFINITY }.uniq.reduce(:+)
+puts answer
