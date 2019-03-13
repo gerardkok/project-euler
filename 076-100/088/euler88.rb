9163e7a688f72c22265959fba2a3c004c408bd8d
Gerard Kok <gerard@gerardkok.nl>
2019-03-13 18:48:38 +0100

    working version

--- 076-100/088/euler88.rb
+++ 076-100/088/euler88.rb
@@ -1,20 +1,39 @@
-require 'set'
+require 'prime'
 
-MAX_K = 6
+MAX_K = 12_000
 MAX_N = 2 * MAX_K
 
 factors = (2..MAX_N).each_with_object(Array.new(MAX_N + 1) { |_| [] }) do |n, acc|
-  max_power = (Math.log(MAX_N) / Math.log(n)).to_i
-  (1..max_power).each do |power|
-    factor = n**power
-    factorization = [n] * power
-    (factor..MAX_N).step(factor).each_with_index do |f, i|
-      next if i + 1 < n
-      acc[f] << [*factorization, i + 1]
+  (n..MAX_N).step(n) do |f|
+    acc[f] << n
+  end
+end
+
+#puts factors.to_s
+
+factorizations = (1..MAX_N).each_with_object({}) do |n, acc|
+  acc[n] = [[n]]
+  factors[n].each do |f|
+    acc[n / f].each do |i|
+      acc[n] << i + [f] if i.none? { |j| j < f }
     end
   end
 end
 
-factors.each_with_index do |f, i|
-  puts "[#{i}]: #{f.to_a}"
+#puts factorizations.to_s
+
+def k(factorization)
+  factorization.length - factorization.reduce(:+) + factorization.reduce(:*)
 end
+
+k = factorizations.each_with_object(Array.new(MAX_K + 1, Float::INFINITY)) do |(i, f), acc|
+  f.each do |factorization|
+    next if factorization.length == 1
+    k_fact = k(factorization)
+    acc[k_fact] = i if k_fact <= MAX_K && i < acc[k_fact]
+  end
+end
+
+#puts k.to_s
+
+puts k.select { |i| i < Float::INFINITY }.uniq.reduce(:+)
