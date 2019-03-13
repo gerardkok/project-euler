a493d7d3ec885989822d632742c4722a20e66da8
Gerard Kok <gerard@gerardkok.nl>
2019-03-13 11:41:46 +0100

    faster solution for 82

--- 076-100/082/euler82.rb
+++ 076-100/082/euler82.rb
@@ -1,21 +1,20 @@
 require 'csv'
 
 def min_path_length(matrix)
-  size = matrix.length # assuming square matrix
-  last_row_lengths = matrix.reduce do |result, row|
-    (0...size).each_with_object(Array.new(size) { |i| row[i] + result[i] }) do |i, r|
-      range_sum = row[i]
-      (i + 1...size).each do |j|
-        range_sum += row[j]
-        r[i] = range_sum + result[j] if range_sum + result[j] < r[i]
-        r[j] = range_sum + result[i] if range_sum + result[i] < r[j]
-      end
+  size = matrix.length  # assuming square matrix
+  min_paths_per_row = (size - 2).downto(0).each_with_object(matrix.map(&:last)) do |column, acc|
+    acc[0] += matrix.first[column]
+    (0...size).each_cons(2) do |upper, row|
+      acc[row] = [acc[upper], acc[row]].min + matrix[row][column]
+    end
+    (size - 1).downto(0).each_cons(2) do |lower, row|
+      acc[row] = [acc[row], acc[lower] + matrix[row][column]].min
     end
   end
-  last_row_lengths.min
+  min_paths_per_row.min
 end
 
-matrix = CSV.read('input82.txt').map { |line| line.map(&:to_i) }.transpose
+matrix = CSV.read('input82.txt').map { |line| line.map(&:to_i) }
 
 answer = min_path_length(matrix)
 
