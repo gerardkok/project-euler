a2c993b5f253e25bcb035c421ee10cf864017203
Gerard Kok <gerard@gerardkok.nl>
2019-03-23 16:05:36 +0100

    cosmetic

--- 051-075/072/euler72.rb
+++ 051-075/072/euler72.rb
@@ -4,7 +4,7 @@ sieve = (2..N).each_with_object((0..N).to_a) do |n, result|
   next unless result[n] == n
 
   result[n] -= 1
-  (2 * n..N).step(n).each do |p|
+  (2 * n..N).step(n) do |p|
     result[p] = (result[p] * (n - 1)) / n
   end
 end
