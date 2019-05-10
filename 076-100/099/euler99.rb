ac05166cfcb9d2fa95ee93235126a33b24b1b72d
Gerard Kok <gerard@gerardkok.nl>
2019-05-10 19:48:16 +0200

    minor improvement

--- 076-100/099/euler99.rb
+++ 076-100/099/euler99.rb
@@ -1,6 +1,4 @@
-lines = File.open('input99.txt').readlines.map(&:chomp)
-
-logs = lines.map { |line| line.split(',').map(&:to_i) }.map { |a, b| b * Math.log(a) }
+logs = File.open('input99.txt').map { |line| line.split(',').map(&:to_i) }.map { |a, b| b * Math.log(a) }
 
 answer = logs.unshift(0).each_with_index.max_by { |n, _| n }.last
 
