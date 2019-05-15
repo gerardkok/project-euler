a8049529fa5182784509e6f11820d892e5a272d1
Gerard Kok <gerard@gerardkok.nl>
2019-05-15 10:37:29 +0200

    improving 103

--- 101-125/103/euler103.rb
+++ 101-125/103/euler103.rb
@@ -21,6 +21,8 @@ class Array
   end
 
   def special?
+    return true if size <= 2
+
     !smaller_subsets_with_bigger_sums? && !subsets_with_equal_sums?
   end
 
@@ -32,28 +34,48 @@ end
 class SumSet
   include Enumerable
 
-  def initialize(size, sum, minimum_element = 1)
+  def initialize(size, sum, set_prefix = [])
     @size = size
     @sum = sum
-    @minimum_element = minimum_element
+    @set_prefix = set_prefix
   end
 
   def each
     if @size == 1
-      yield [@sum]
+      yield @set_prefix if @set_prefix.push(@sum).special?
     else
+      min = @set_prefix.empty? ? @size - 1 : @set_prefix.last + 1
       max = (@sum - (@size * (@size - 1) >> 1)) / @size
-      (@minimum_element..max).each do |i|
-        SumSet.new(@size - 1, @sum - i, i + 1).each do |j|
-          yield j.unshift(i)
+      (min..max).each do |i|
+        s = @set_prefix + [i]
+        next unless s.special?
+
+        self.class.new(@size - 1, @sum - i, s).each do |j|
+          yield j
         end
       end
     end
   end
 end
 
+def special_sum_sets(size, sum, set_prefix = [])
+  if size == 1
+    s = set_prefix + [sum]
+    s.special? ? [s] : []
+  else
+    min = set_prefix.empty? ? size - 1 : set_prefix.last + 1
+    max = (sum - (size * (size - 1) >> 1)) / size
+    (min..max).each_with_object([]) do |i, memo|
+      s = set_prefix + [i]
+      next unless s.special?
+
+      memo.concat(special_sum_sets(size - 1, sum - i, s))
+    end
+  end
+end
+
 def find_sorted_sum_set(sum)
-  SumSet.new(TARGET_SIZE, sum).find(&:special?)
+  SumSet.new(TARGET_SIZE, sum).find(&:any?)
 end
 
 start = [20, 31, 38, 39, 40, 42, 45].sum - 1
