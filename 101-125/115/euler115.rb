c9adb818bb1fab9fe41fee24acf44f4cd1e02e36
Gerard Kok <gerard@gerardkok.nl>
2019-06-02 10:36:35 +0200

    114 and 115

---
+++ 101-125/115/euler115.rb
@@ -0,0 +1,17 @@
+number_of_block_combinations = Hash.new do |h, (remaining_row_length, minimum_block_size)|
+  h[[remaining_row_length, minimum_block_size]] =
+    if minimum_block_size > remaining_row_length
+      1
+    else
+      (0..remaining_row_length).reduce(1) do |remaining_row_sum, r|
+        block_size_combinations = (minimum_block_size..remaining_row_length - r).reduce(0) do |block_size_sum, b|
+          block_size_sum + h[[remaining_row_length - r - b - 1, minimum_block_size]]
+        end
+        remaining_row_sum + block_size_combinations
+      end
+    end
+end
+
+answer = 1.step.find { |n| number_of_block_combinations[[n, 50]] > 1_000_000 }
+
+puts answer
