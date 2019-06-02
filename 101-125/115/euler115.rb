eef93fcda4d71888ec0f9146343c2ca09c538031
Gerard Kok <gerard@gerardkok.nl>
2019-06-02 10:58:28 +0200

    even more readable (hopefully)

--- 101-125/115/euler115.rb
+++ 101-125/115/euler115.rb
@@ -1,15 +1,19 @@
 BLOCK_SIZE = 50
 
+class Array
+  def sum
+    reduce(0, :+)
+  end
+end
+
 number_of_block_combinations = Hash.new do |h, row_length|
-  h[row_length] =
+  remaining_combinations =
     if row_length < BLOCK_SIZE
-      1
+      0
     else
-      row_length_combinations = (0..row_length).map do |r|
-        (BLOCK_SIZE..row_length - r).map { |b| h[row_length - r - b - 1] }.reduce(0, :+)
-      end
-      row_length_combinations.reduce(1, :+)
+      (0..row_length).map { |r| (BLOCK_SIZE..row_length - r).map { |b| h[row_length - r - b - 1] }.sum }.sum
     end
+  h[row_length] = 1 + remaining_combinations
 end
 
 answer = 1.step.find { |n| number_of_block_combinations[n] > 1_000_000 }
