1a1a5b4eb2a24f44f352871850e71a9e00f514b0
Gerard Kok <gerard@gerardkok.nl>
2019-12-22 13:51:29 +0100

    problem 116

---
+++ 101-125/116/euler116.rb
@@ -0,0 +1,32 @@
+class Array
+  def sum
+    reduce(0, :+)
+  end
+end
+
+def number_of_combinations(cached_combinations, row_length, block_size)
+  return 0 if row_length < block_size
+
+  (0..row_length - block_size).map { |r| cached_combinations[row_length - block_size - r] + 1 }.sum
+end
+
+number_of_red_combinations = Hash.new do |h, row_length|
+  h[row_length] = number_of_combinations(h, row_length, 2)
+end
+
+number_of_green_combinations = Hash.new do |h, row_length|
+  h[row_length] = number_of_combinations(h, row_length, 3)
+end
+
+number_of_blue_combinations = Hash.new do |h, row_length|
+  h[row_length] = number_of_combinations(h, row_length, 4)
+end
+
+number_of_block_combinations = Hash.new do |h, row_length|
+  h[row_length] =
+    number_of_red_combinations[row_length] + number_of_green_combinations[row_length] + number_of_blue_combinations[row_length]
+end
+
+answer = number_of_block_combinations[50]
+
+puts answer
