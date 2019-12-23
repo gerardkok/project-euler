e4efc5928e83bafbcb2be5044a6577986e4eada6
Gerard Kok <gerard@gerardkok.nl>
2019-12-23 20:48:12 +0100

    simplifying 116

--- 101-125/116/euler116.rb
+++ 101-125/116/euler116.rb
@@ -7,20 +7,14 @@ end
 def number_of_combinations(cached_combinations, row_length, block_size)
   return 0 if row_length < block_size
 
-  (0..row_length - block_size).map { |r| cached_combinations[row_length - block_size - r] + 1 }.sum
+  (0..row_length - block_size).map { |r| cached_combinations[r] + 1 }.sum
 end
 
-number_of_red_combinations = Hash.new do |h, row_length|
-  h[row_length] = number_of_combinations(h, row_length, 2)
-end
+number_of_red_combinations = Hash.new { |h, row_length| h[row_length] = number_of_combinations(h, row_length, 2) }
 
-number_of_green_combinations = Hash.new do |h, row_length|
-  h[row_length] = number_of_combinations(h, row_length, 3)
-end
+number_of_green_combinations = Hash.new { |h, row_length| h[row_length] = number_of_combinations(h, row_length, 3) }
 
-number_of_blue_combinations = Hash.new do |h, row_length|
-  h[row_length] = number_of_combinations(h, row_length, 4)
-end
+number_of_blue_combinations = Hash.new { |h, row_length| h[row_length] = number_of_combinations(h, row_length, 4) }
 
 number_of_block_combinations = Hash.new do |h, row_length|
   h[row_length] =
